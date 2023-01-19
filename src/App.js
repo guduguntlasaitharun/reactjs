8c1a5c1d5232e59457d82a7dd37bff99d793e342
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2023-01-19 22:46:57 +0530

    react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         WELCOME TO MY HOME
         </p>
         <a
           className="App-link"
